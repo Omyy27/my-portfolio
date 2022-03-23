6bc26e6baef063cefc2cb7e71c10ca24f2afb643
Omyy27 <omovisolutions@gmail.com>
2022-03-22 20:13:41 -0500

    Email text

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,10 +7,10 @@ const Header = () => {
     <>
       <Navbar bg="white" expand="lg" fixed="top">
         <Container>
-          <Navbar.Brand href="mailto:omovisolutions@gmail.com" className="mx-2"><UilEnvelope  /> <span className="email-text">omovisolutions@gmail.com</span> </Navbar.Brand>
+          <Navbar.Brand href="mailto:omovisolutions@gmail.com" className="mx-2"><UilEnvelope  /> <span className="email-text">omdev@omprojects.com</span> </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            <Nav>
+            <Nav className="fw-bold">
               <Nav.Link href="#resume">Resume</Nav.Link> 
               <Nav.Link href="#works">Works</Nav.Link >
               <Nav.Link href="#contact">Contact</Nav.Link> 
